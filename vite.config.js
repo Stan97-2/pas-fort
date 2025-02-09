c3f7e7b08c2eee396b5b57a6955eb06d537ffedc
Stan97-2 <stan.dellon972@gmail.com>
2025-02-09 17:07:18 +0100

    fix config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/pas-fort/"
+  base: "/pas-fort/",
   plugins: [
     vue(),
     vueDevTools(),
