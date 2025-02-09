cc1e4170f88859bb8f0f8a6ad9ddea682cf6667c
Stan97-2 <stan.dellon972@gmail.com>
2025-02-09 17:50:50 +0100

    styling

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,6 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo_pas_fort.png" width="125" height="125" />
-
     <div class="wrapper">
       <HelloWorld msg="Classement officiel des joueurs les moins forts" />
 
